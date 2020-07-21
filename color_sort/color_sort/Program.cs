c70ee53a2154566667994428bcea23f17a3c6460
Daria-Shishigina <42940604+Daria-Shishigina@users.noreply.github.com>
2020-07-21 17:08:36 +0700

    Add files via upload

---
+++ color_sort/color_sort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        private static string SortRGB(string source)
+        {
+            var dict = new Dictionary<char, int>()
+ {
+ {
+ 'g', 0
+ },
+ {
+ 'b', 1
+ },
+ {
+ 'r', 2
+ }
+ };
+            var list = source.ToList().FindAll(a => a == 'r' || a == 'b' || a == 'g');
+            list.Sort((a, b) => dict[a].CompareTo(dict[b]));
+            // G < B < R 
+            return new string(list.ToArray());
+        }
+
+        static void Main(string[] args)
+        {
+            Test();
+
+            var str = Console.ReadLine();
+            Console.WriteLine(SortRGB(str));
+            Console.ReadKey();
+        }
+
+
+        static void Test()
+        {
+            var str = "grbbbg";
+            var result = SortRGB(str);
+            var actual = "ggbbbr";
+            var strscf = actual == result ? "Successful " : "not successful";
+            Console.WriteLine($"Source {str}. Dest {result}. Actual {actual}. Test {strscf}");
+
+        }
+    }
+}
+
+
